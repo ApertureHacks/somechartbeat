041c0ecc92d19ffd9b8deff71b203fcfcf3eb377
Josh Matthews <josh@jmatthews.us>
2013-10-13 09:23:37 -0400

    Fixing POST access in chartbeat.

--- routes/chartbeat.js
+++ routes/chartbeat.js
@@ -1,13 +1,13 @@
 var request = require('request');
 
 exports.fetch = function(req, res) {
-  var card_url = req.params.url;
+  var card_url = req.body.card_url;
 
-  var request_url = chartbeat_endpoint +
-    "?apikey=" + chartbeat_key + "&host=someecards.com&path=" + card_url;
+  var request_url = chartbeat_endpoint + "?apikey=" + chartbeat_key + "&host=someecards.com&path=" + card_url;
 
+  console.log("requesting with: " + request_url);
   request(request_url, function(err, resp, body) {
-    if (!error && response.statusCode == 200) {
+    if (!err && resp.statusCode == 200) {
       res.write(body.toString());
       res.end();
     }
